bf53327c4d2003bd8c913e164f8936cd0eed5167
Asnur Ramdani <asnurramdhani12@gmail.com>
2023-02-13 21:17:34 +0700

    add challenge for audience

---
+++ basic/examples/challange_1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		// Buat Kode Untuk Menampilkan Angka 1 - 10 dengan kategori ganjil dan genap
+		// Contoh Output:
+		// 1 - Ganjil
+		// 2 - Genap
+		// 3 - Ganjil
+		// dst
+		fmt.Println(i)
+		// Clue Challenge 1:
+		// Gunakan if else
+		// Gunakan operator modulus
+	}
+}
